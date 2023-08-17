f666e4123f7ae15f8b528857508b549bc9f4f5d7
BiancaMe <bianquis0410@gmail.com>
2023-08-17 18:03:13 -0300

    Changes required

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> Math Magicians</p>
-      </header>
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p> Math Magicians</p>
+    </header>
+    <Calculator />
+  </div>
+);
 
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,24 +3,15 @@ import DisplayOutPut from './Display';
 import Buttons from './Buttons';
 import '../style/calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="app">
-        <div className="calculator">
-          <DisplayOutPut />
-          <div className="buttons">
-            <Buttons />
-          </div>
-        </div>
+const Calculator = () => (
+  <div className="app">
+    <div className="calculator">
+      <DisplayOutPut />
+      <div className="buttons">
+        <Buttons />
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default Calculator;
