8f9c0a3ef44cd11ec4845c0ff1dc0de589f353b9
BiancaMe <bianquis0410@gmail.com>
2023-08-17 16:31:15 -0300

    Update desing, fix linters errors

--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Button = (props) => (
-  <input className="button" type="button" value={props.label} />
-);
+export const Button = (props) => {
+  const { label } = props;
+  return (
+    <input className="button" type="button" value={label} />
+  );
+};
 
-export const Operation = (props) => (
-  <input className="button operation" type="button" value={props.label} />
-);
+export const Operation = (props) => {
+  const { label } = props;
+  return (
+    <input className="button operation" type="button" value={label} />
+  );
+};
 
 Button.propTypes = {
+  label: PropTypes.string.isRequired,
+};
+
+Operation.propTypes = {
   label: PropTypes.string.isRequired,
 };
