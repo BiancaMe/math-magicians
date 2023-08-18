a462b32bb28961c61ba94d0ca2edb77ee86a0d04
BiancaMe <bianquis0410@gmail.com>
2023-08-18 20:10:21 -0300

    Add events

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,38 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, Operation } from './button';
 
-const Buttons = () => (
-  <>
-    <div className="row">
-      <Button label="AC" />
-      <Button label="+/-" />
-      <Button label="%" />
-      <Operation label="+" />
+const Buttons = (props) => {
+  const { click, key } = props;
+  return (
+    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+    <div className="buttons" onClick={click} onKeyDown={key}>
+      <div className="row">
+        <Button label="AC" />
+        <Button label="+/-" />
+        <Button label="%" />
+        <Operation label="÷" />
+      </div>
+      <div className="row">
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Operation label="x" />
+      </div>
+      <div className="row">
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Operation label="-" />
+      </div>
+      <div className="row">
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Operation label="+" />
+      </div>
+      <div className="row last-row">
+        <Button label="0" />
+        <Button label="." />
+        <Operation label="=" />
+      </div>
     </div>
-    <div className="row">
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Operation label="*" />
-    </div>
-    <div className="row">
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Operation label="-" />
-    </div>
-    <div className="row">
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Operation label="+" />
-    </div>
-    <div className="row last-row">
-      <Button label="0" />
-      <Button label="." />
-      <Operation label="=" />
-    </div>
-  </>
-);
+  );
+};
+
+Buttons.propTypes = {
+  click: PropTypes.func.isRequired,
+  key: PropTypes.func,
+};
+
+Buttons.defaultProps = {
+  key: () => -1,
+};
 
 export default Buttons;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DisplayOutPut from './Display';
 import Buttons from './Buttons';
+import calculate from '../logic/calculate';
+
 import '../style/calculator.css';
 
-const Calculator = () => (
-  <div className="app">
-    <div className="calculator">
-      <DisplayOutPut />
-      <div className="buttons">
-        <Buttons />
+const Calculator = () => {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setObj(calculate(obj, e.target.value));
+  };
+
+  return (
+    <div className="app">
+      <div className="calculator">
+        <DisplayOutPut total={obj.total || ''} operation={obj.operation || ''} next={obj.next || ''} />
+        <Buttons click={(e) => handleClick(e)} />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
