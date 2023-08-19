17b5c534d0d1f74b6e7fb16148ef172e97e821e1
BiancaMe <bianquis0410@gmail.com>
2023-08-19 15:57:54 -0300

    Add quote

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import Calculator from './components/Calculator';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CalculatorNav from './routes/calculatorSec';
+import QuoteNav from './routes/quoteSec';
+import HomeNav from './routes/homSec';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p> Math Magicians</p>
-    </header>
-    <Calculator />
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomeNav />} />
+      <Route path="/calculator" element={<CalculatorNav />} />
+      <Route path="/quote" element={<QuoteNav />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
---
+++ src/routes/quoteSec.js
@@ -0,0 +1,14 @@
+import Navbar from '../components/navbar';
+import Quote from '../components/Quote';
+
+const QuoteNav = () => (
+  <div className="App">
+    <header className="App-header">
+      <p> Math Magicians</p>
+      <Navbar />
+    </header>
+    <Quote />
+  </div>
+);
+
+export default QuoteNav;
